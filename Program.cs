573d097314cf39a3683f6b674af827aa03e0fc59
PRAKTIKAN <79889727+Raychan69@users.noreply.github.com>
2023-03-20 07:16:07 +0700

    “menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Penjumlahan<int> obj = new Penjumlahan<int>();
+
+            obj.JumlahTigaAngka(13,2,21);
         }
     }
+
+    class Penjumlahan<T>
+    {
+        public void JumlahTigaAngka(T input1,T input2,T input3)
+        {
+            dynamic input1_d = input1;
+            dynamic input2_d = input2;
+            dynamic input3_d = input3;
+            Console.WriteLine(input1_d + input2_d + input3_d);
+        }
+    }
+
+   
 }
